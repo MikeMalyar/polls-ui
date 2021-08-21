351663c43395597b189258020df63324d98521df
MykeMalyar <michaelbodybuilder1999@gmail.com>
2021-08-21 11:31:39 +0300

    Added left bar for main page. Added bootstrap icons

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MyGroupDto, MyPollDto} from '../models/dto';
 
 @Component({
   selector: 'app-main-page',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainPageComponent implements OnInit {
 
-  constructor() { }
+  userName: string;
+  myPolls: MyPollDto[] = [];
+  myGroups: MyGroupDto[] = [];
+
+  constructor() {
+  }
 
   ngOnInit() {
+    this.userName = 'Mykhailo Maliarchuk'; // take from DB
+
+    this.myPolls.push(new MyPollDto(1, 'Тестове опитування 1', 3),
+      new MyPollDto(2, 'Тестове опитування номер 2', 20));   // take from DB
+
+    this.myGroups.push(new MyGroupDto(1, '602', 10),
+      new MyGroupDto(2, '607', 4));   // take from DB
   }
 
 }
--- src/app/models/dto.ts
+++ src/app/models/dto.ts
@@ -10,3 +10,27 @@ export class RegistrationDto {
   email: string;
   fullName: string;
 }
+
+export class MyPollDto {
+  id: bigint;
+  title: string;
+  reacted: number;
+
+  constructor(id, title, reacted) {
+    this.id = id;
+    this.title = title;
+    this.reacted = reacted;
+  }
+}
+
+export class MyGroupDto {
+  id: bigint;
+  name: string;
+  newContent: number;
+
+  constructor(id, name, newContent) {
+    this.id = id;
+    this.name = name;
+    this.newContent = newContent;
+  }
+}
