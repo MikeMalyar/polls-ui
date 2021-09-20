15e572756aa4a3bf4cf9b2bc3411ac5406974a7a
MykeMalyar <michaelbodybuilder1999@gmail.com>
2021-09-20 16:36:31 +0300

    Added register logic to call backend

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,5 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {RegistrationDto} from '../models/dto';
+import {HttpClient} from '@angular/common/http';
+import {Router} from '@angular/router';
+import {GenericResponse} from '../models/rest';
+import {HTTP_OPTIONS, SERVER_URL} from '../config/http-config';
 
 @Component({
   selector: 'app-registration',
@@ -10,10 +14,23 @@ export class RegistrationComponent implements OnInit {
 
   registrationDto = new RegistrationDto();
 
-  constructor() {
+  errorMessage: string;
+
+  constructor(private http: HttpClient, private router: Router) {
   }
 
   ngOnInit() {
+  }
+
+  register() {
+    this.http.post<GenericResponse>(SERVER_URL + '/user/register', this.registrationDto, HTTP_OPTIONS)
+      .subscribe(data => {
+        if (data.success) {
+          this.router.navigate(['/login']);
+        } else {
+          this.errorMessage = data.message;
+        }
+      }, error => console.error(error));
   }
 
 }
