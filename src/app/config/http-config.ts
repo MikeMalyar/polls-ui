8cca5860b590864e33079353490c8f05893be9df
MykeMalyar <michaelbodybuilder1999@gmail.com>
2021-11-22 14:02:51 +0200

    Changes for Heroku

--- src/app/config/http-config.ts
+++ src/app/config/http-config.ts
@@ -1,12 +1,12 @@
 import {HttpHeaders} from '@angular/common/http';
 
-export const SERVER_URL = 'https://poll-chnu-backend.herokuapp.com';
-export const SELF_URL = 'https://poll-chnu-ui.herokuapp.com/';
+export const SERVER_URL = 'https://polls-chnu-backend.herokuapp.com';
+export const SELF_URL = 'https://polls-chnu-ui.herokuapp.com/';
 
 export const HTTP_OPTIONS = {
   headers: new HttpHeaders({
     'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': 'https://poll-chnu-ui.herokuapp.com',
+    'Access-Control-Allow-Origin': 'https://polls-chnu-ui.herokuapp.com',
     'Access-Control-Allow-Credentials': 'true',
   }), withCredentials: true
 };
