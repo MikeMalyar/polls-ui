3f49c4c3e914b1c1511f2fa1011b7867e33fd63b
MykeMalyar <michaelbodybuilder1999@gmail.com>
2021-12-03 16:02:26 +0200

    Changes for Heroku

--- src/app/config/http-config.ts
+++ src/app/config/http-config.ts
@@ -6,7 +6,7 @@ export const SELF_URL = 'https://polls-chnu-ui.herokuapp.com/';
 export const HTTP_OPTIONS = {
   headers: new HttpHeaders({
     'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': 'https://polls-chnu-ui.herokuapp.com',
+    'Access-Control-Allow-Origin': SELF_URL,
     'Access-Control-Allow-Credentials': 'true',
     'Access-Control-Allow-Headers': 'content-type,withcredentials,set-cookie',
   }), withCredentials: true
